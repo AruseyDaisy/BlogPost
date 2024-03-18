26bbbffa96f662ea2fe69adce1641d5cdb5af6a4
AruseyDaisy <daisyarusey.dc@gmail.com>
2024-03-18 15:32:29 +0300

    Ignore files

---
+++ Blog_post/Services/BlogService.cs
@@ -0,0 +1,21 @@
+﻿using Blog_post_API.Models;
+using System.Net.Http.Json;
+
+namespace Blog_post.Services
+{
+    public class BlogService : IBlogService
+    {
+        private readonly HttpClient _http;
+        public BlogService(HttpClient http)
+        {
+            _http = http;
+        }
+        public async Task<List<Blog>> GetBlogs()
+        {
+
+
+            var response = await _http.GetFromJsonAsync<List<Blog>>("/api/Blogs");
+            return response;
+        }
+    }
+}
---
+++ Blog_post/Services/IBlogService.cs
@@ -0,0 +1,9 @@
+﻿using Blog_post_API.Models;
+
+namespace Blog_post.Services
+{
+    public interface IBlogService
+    {
+        public Task<List<Blog>> GetBlogs();
+    }
+}
